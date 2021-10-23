8dda2bfc74b99355585f3a760974db858b244318
Oleg Kirsanov <olegkirsanov22@gmail.com>
2021-10-23 21:20:13 +0300

    Create empty collection view in AlbumsCollectionViewController.

--- AlbumsCollectionProject/Sources/Modules/Albums/View/AlbumsViewController.swift
+++ AlbumsCollectionProject/Sources/Modules/Albums/View/AlbumsViewController.swift
@@ -9,8 +9,29 @@ import Foundation
 import UIKit
 
 final class AlbumsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.backgroundColor = .white
+        title = "Альбомы"
+
+        configureCollectionView()
+    }
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .white
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 5).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+        collectionView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
     }
 }
